4c4f62b47923af1767af3094362e191d349b319c
Fernando Molano <fm87usa@gmail.com>
2021-10-01 22:30:32 -0500

    Fix issue that broke program when user entered string or a number different from answer options, add gitignore file

--- ui.py
+++ ui.py
@@ -13,6 +13,9 @@ def display_all_categories():
 
 
 def get_questions(topic):
+    total_points_available = 0
+    total_points_obtained = 0
+
     questions_list = get_topic_id(topic)
 
     print("\nQUESTIONS:\n")
@@ -20,17 +23,28 @@ def get_questions(topic):
         difficulty, points_available = get_points(question['difficulty'])
         print(
             f"Difficulty: {difficulty}\tPoints available: {points_available}")
+        total_points_available += points_available
+
         print(question['question'])
         print()
         print("Options:")
         print(
             f"1) {question['answer_1']}\t2) {question['answer_2']}\t3) {question['answer_3']}\t4) {question['answer_4']}")
-        user_answer = input("\nEnter number of option: ")
+
+        user_answer = input("\nEnter number of answer: ")
         try:
+            int(user_answer)
+            while int(user_answer) < 0 or int(user_answer) > 4:
+                user_answer = input(
+                    "\nAnswer chosen must be between 1 and 4: ")
+
             user_answer_value = question[f'answer_{user_answer}']
+            print(user_answer_value)
             if user_answer_value == question['answer_1']:
+                total_points_obtained += points_available
                 print("Correct\n\n")
             else:
                 print("Wrong\n\n")
+
         except ValueError as err:
-            print(err)
+            user_answer = input("\nAnswer chosen must be between 1 and 4: ")
