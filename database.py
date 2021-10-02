1ae87f1eb1274581d395108c8ef70e6f2705ff32
Fernando Molano <fm87usa@gmail.com>
2021-10-02 15:39:19 -0500

    delete database.py file, functionality has been moved to db_manager to easily access them

--- database.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-# Connect to DB and create cursor to execute queries
-with sqlite3.connect('./database/quizzes.db') as conn:
-    conn.row_factory = sqlite3.Row  # Upgrade row_factory
-    cursor = conn.cursor()
