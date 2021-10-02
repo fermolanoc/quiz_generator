627c237fc76b58f991462e8c7957912f5fb313a7
Fernando Molano <fm87usa@gmail.com>
2021-10-01 20:42:57 -0500

    Fix single_question dictionary on key names to make it easier to evaluate user answers against key:value

--- db_manager.py
+++ db_manager.py
@@ -1,7 +1,48 @@
 from database import cursor
 
-data = cursor.execute("Select * from questions")
-# data = cursor.execute(
-#     "Select distinct(c.name) from categories c inner join questions q on c.id = q.category_id where category_id = 2")
-for result in data:
-    print(result)
+
+def get_categories():
+    all_categories = cursor.execute("Select name from categories")
+
+    # print(all_categories)
+    return all_categories
+
+
+def get_questions(topic_id):
+    questions = cursor.execute(
+        f'Select * from questions q inner join categories c on q.category_id = c.id where q.category_id = {topic_id}')
+
+    questions_list = []
+    for item in questions:
+        single_question = {}
+        text = item['question']
+        correct_answer_1 = item['correct_answer']
+        wrong_answer_2 = item['wrong_answer_1']
+        wrong_answer_3 = item['wrong_answer_2']
+        wrong_answer_4 = item['wrong_answer_3']
+
+        # print(text, option_1, option_2)
+
+        single_question['question'] = text,
+        single_question['answer_1'] = correct_answer_1
+        single_question['answer_2'] = wrong_answer_2
+        single_question['answer_3'] = wrong_answer_3
+        single_question['answer_4'] = wrong_answer_4
+
+        # print(single_question)
+        questions_list.append(single_question)
+    # print(questions_list)
+    return questions_list
+
+
+def get_topic_id(topic):
+    topic_id = cursor.execute(
+        f'Select id from categories where categories.name = \'{topic}\'')
+
+    for row in topic_id:
+        questions_list = get_questions(row['id'])
+        # print(questions_list)
+        return questions_list
+
+
+get_topic_id('Entertainment')
